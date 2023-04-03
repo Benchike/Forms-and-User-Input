272efc36a21fba7dde600a7041802a1d4d4267b9
benchike <benchike10@gmail.com>
2023-04-03 01:27:32 +0100

    beta commit

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,23 +2,17 @@ import { useState } from 'react';
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('');
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
-
-    if (event.target.value.trim() !== '') {
-      setEnteredNameIsValid(true);
-    }
   };
 
   const nameInputBlurHandler = (event) => {
     setEnteredNameTouched(true);
-
-    if (enteredName.trim() === '') {
-      setEnteredNameIsValid(false);
-    }
   };
 
   const formSubmissionHandler = (event) => {
@@ -26,17 +20,15 @@ const SimpleInput = (props) => {
 
     setEnteredNameTouched(true);
 
-    if (enteredName.trim() === '') {
-      setEnteredNameIsValid(false);
+    if (!enteredNameIsValid) {
       return;
     }
 
-    setEnteredNameIsValid(true);
-
     console.log(enteredName);
-  };
 
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+    setEnteredName('');
+    setEnteredNameTouched(false);
+  };
 
   const nameInputClasses = nameInputIsInvalid
     ? 'form-control invalid'
